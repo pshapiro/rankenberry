60cd33b58ceaa934dac2e674c96dfa82532d222b
Paul <paul@pauls-mbp-3.mynetworksettings.com>
2024-09-01 23:05:38 -0400

    Added a logo. Very demure. Very cutesy.

--- frontend/rankenberry-frontent/src/App.vue
+++ frontend/rankenberry-frontent/src/App.vue
@@ -15,12 +15,13 @@ watch(() => route.meta.title, (newTitle) => {
     <nav class="navbar is-light" role="navigation" aria-label="main navigation">
       <div class="navbar-brand">
         <a class="navbar-item" href="/">
+          <img src="@/assets/rankenberry-logo.png" alt="Rankenberry Logo" class="logo">
           <span class="has-text-weight-bold is-size-4">Rankenberry</span>
         </a>
       </div>
       <div class="navbar-menu">
         <div class="navbar-start">
-          <router-link to="/" class="navbar-item">Home / Rankings</router-link>
+          <router-link to="/" class="navbar-item">Home / Rank Table</router-link>
           <router-link to="/add" class="navbar-item">Add Project/Keywords</router-link>
           <router-link to="/keyword-management" class="navbar-item">Keyword Management</router-link>
         </div>
@@ -48,5 +49,25 @@ watch(() => route.meta.title, (newTitle) => {
   padding: 2rem;
   max-width: 1200px;
   margin: 0 auto;
+}
+
+.navbar-brand {
+  display: flex;
+  align-items: center;
+}
+
+.navbar-brand .logo {
+  width: auto;
+  height: 42px;
+  margin-right: 0; /* Removed margin */
+}
+
+.navbar-brand span {
+  color: inherit;
+}
+
+/* Adjust padding for the navbar-item */
+.navbar-brand .navbar-item {
+  padding: 5px 10px 5px 10px; /* Reduced right padding */
 }
 </style>
